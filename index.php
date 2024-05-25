565ba3869cf84316b36e4b4c24bbb1522a9704e3
NHMCemter <146455114+NHMCemter@users.noreply.github.com>
2024-05-25 12:25:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Rapat Univeristas Nurul Huda</title>
+	<link rel="stylesheet" type="text/css" href="login/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<img class="wave" src="login/img/wave.png">
+	<div class="container">
+		<div class="img">
+			<img src="login/img/bg.svg">
+		</div>
+		<div class="login-content">
+			<form action="/rapat_unuha/fungsi/cek_login.php" method="POST">
+				<img src="login/img/avatar.svg">
+				<h2 class="title">Selamat Datang</h2>
+				<div class="input-div one">
+					<div class="i">
+						<i class="fas fa-user"></i>
+					</div>
+					<div class="div">
+						<h5>Username</h5>
+						<input name="username" type="text" class="input">
+					</div>
+				</div>
+				<div class="input-div pass">
+					<div class="i">
+						<i class="fas fa-lock"></i>
+					</div>
+					<div class="div">
+						<h5>Password</h5>
+						<input name="password" type="password" class="input">
+					</div>
+				</div>
+				<a href="#">Forgot Password?</a>
+				<input type="submit" class="btn" value="Login">
+			</form>
+		</div>
+	</div>
+	<script type="text/javascript" src="login/js/main.js"></script>
+</body>
+
+</html>
